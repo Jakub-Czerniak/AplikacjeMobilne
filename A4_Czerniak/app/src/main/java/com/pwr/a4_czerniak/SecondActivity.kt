2d0d75496afbbc0dde7411b14d46b0f2b440f839
Kuba <kubaczarny3@gmail.com>
2022-05-13 19:01:28 +0200

    Lab4

---
+++ A4_Czerniak/app/src/main/java/com/pwr/a4_czerniak/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.pwr.a4_czerniak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        textHello.text = message
+    }
+}
